6b95693665ef7c33b27aff0e35d1b2b5c74d8a08
Francisco López Toledo <francisco.lopezt@um.es>
2021-03-22 23:11:37 +0100

    synchronous version

---
+++ src/ga.c
@@ -0,0 +1,244 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <mpi.h>
+#include "../include/derivados_mpi.h"
+#include "../include/imagen.h"
+#include "../include/ga.h"
+
+#define PRINT 0
+
+extern int rank, world_size;
+extern MPI_Datatype rgb_type, individuo_type;
+
+static int aleatorio(int max) {
+	return (rand() % (max+1));
+}
+
+void init_imagen_aleatoria(RGB *imagen, int max, int total)
+{
+	for(int i = 0; i < total; i++) {
+		imagen[i].r = aleatorio(max);
+		imagen[i].g = aleatorio(max);
+		imagen[i].b = aleatorio(max);
+	}
+}
+
+RGB *imagen_aleatoria(int max, int total) {
+	RGB *imagen = (RGB *) malloc(total*sizeof(RGB));
+	assert(imagen);
+	
+	init_imagen_aleatoria(imagen, max, total);
+	return imagen;
+}
+
+static int comp_fitness(const void *a, const void *b) {
+	/* qsort pasa un puntero al elemento que está ordenando */
+	return (*(Individuo*)a).fitness - (*(Individuo*)b).fitness;
+}
+
+void scatterPoblacion(int *offset, int chunk_size, int leftover, Individuo *pobMaster, Individuo *pobRecv)
+{
+  MPI_Status status;
+  int tagOff = 0, tagPob = 1;
+
+  if(rank == MASTER) {
+   *offset += chunk_size + leftover;
+   for(int i = 1; i < world_size; i++) {
+     MPI_Send(offset, 1, MPI_INT, i, tagOff, MPI_COMM_WORLD);
+     MPI_Send(&(pobMaster[*offset]), chunk_size, individuo_type, i, tagPob, MPI_COMM_WORLD);
+     *offset += chunk_size;
+   }
+  } else {
+   MPI_Recv(offset, 1, MPI_INT, MASTER, tagOff, MPI_COMM_WORLD, &status);
+   MPI_Recv(pobRecv, chunk_size, individuo_type, MASTER, tagPob, MPI_COMM_WORLD, &status);
+  }
+}
+
+void gatherPoblacion(int *offset, int chunk_size, Individuo *pobMaster, Individuo *pobSend)
+{
+  MPI_Status status;
+  int tagOff = 0, tagPob = 1;
+
+  if(rank == MASTER) {
+    for(int i = 1; i < world_size; i++) {
+      MPI_Recv(offset, 1, MPI_INT, MPI_ANY_SOURCE, tagOff, MPI_COMM_WORLD, &status);
+      MPI_Recv(&(pobMaster[*offset]), chunk_size, individuo_type, status.MPI_SOURCE, tagPob, MPI_COMM_WORLD, &status);
+    }
+  } else {
+      MPI_Send(offset, 1, MPI_INT, MASTER, tagOff, MPI_COMM_WORLD);
+      MPI_Send(pobSend, chunk_size, individuo_type, MASTER, tagPob, MPI_COMM_WORLD);
+  }
+}
+
+void crear_imagen(const RGB *imagen_objetivo, int num_pixels, int ancho, int alto, int max, int num_generaciones, int tam_poblacion, RGB *imagen_resultado, const char *output_file)
+{
+  int i, mutation_start;
+  double fitness_anterior, fitness_actual, diferencia_fitness;
+  int chunk_size = tam_poblacion / world_size;
+  int leftover = tam_poblacion % world_size;
+  int offset = 0;
+
+  // Se crea la población inicial
+  Individuo *poblacion = NULL;
+  if(rank == MASTER) {
+    poblacion = (Individuo *) malloc(tam_poblacion * sizeof(Individuo));
+    for(i = 0; i < tam_poblacion; i++) {
+      init_imagen_aleatoria(poblacion[i].imagen, max, num_pixels);
+    }  
+  } else {
+    poblacion = (Individuo *) malloc(chunk_size * sizeof(Individuo));
+  }
+  
+  // Calcular fitness
+  if(rank == MASTER) {
+    offset = 0;
+    scatterPoblacion(&offset, chunk_size, leftover, poblacion, NULL);
+    chunk_size += leftover;
+  } else {
+    scatterPoblacion(&offset, chunk_size, leftover, NULL, poblacion);
+  }
+  for(i = 0; i < chunk_size; i++) {
+    fitness(imagen_objetivo, &poblacion[i], num_pixels);
+  }
+
+  if(rank == MASTER) {
+    chunk_size = tam_poblacion / world_size;
+    gatherPoblacion(&offset, chunk_size, poblacion, NULL);
+    // Se ordenan los individuos según su fitness
+    qsort(poblacion, tam_poblacion, sizeof(Individuo), comp_fitness);
+
+    // Se envía la población ordenada para evolucionarla
+    offset = 0;
+    scatterPoblacion(&offset, chunk_size, leftover, poblacion, NULL);
+    chunk_size += leftover;
+  } else {
+    gatherPoblacion(&offset, chunk_size, NULL, poblacion);
+    
+    // Recibe la población ordenada para su evolución
+    scatterPoblacion(&offset, chunk_size, leftover, NULL, poblacion);
+  }
+  // Evolucionar la población durante un número de generaciones
+  for(int g = 0; g < num_generaciones; g++) {
+    if(rank == MASTER) {
+      fitness_anterior = poblacion[0].fitness;
+    }
+
+    for(i = 0; i < chunk_size/2 - 1; i += 2) {
+      cruzar(&poblacion[i], &poblacion[i+1], &poblacion[chunk_size/2+i], &poblacion[chunk_size/2+i+1], num_pixels);
+    }
+
+    // Mutar una parte de la población
+    mutation_start = chunk_size/4;
+
+    for(i = mutation_start; i < chunk_size; i++) {
+      mutar(&poblacion[i], max, num_pixels);
+    }
+
+    // Recalcular fitness
+      for(i = 0; i < chunk_size; i++) {
+        fitness(imagen_objetivo, &poblacion[i], num_pixels);
+      }
+      if((g % 10 == 0 && g) || g == num_generaciones -1) {
+      
+        if(rank == MASTER) {
+          chunk_size = tam_poblacion / world_size;
+          gatherPoblacion(&offset, chunk_size, poblacion, NULL);
+            // Se ordenan los individuos según su fitness
+          qsort(poblacion, tam_poblacion, sizeof(Individuo), comp_fitness);
+            
+          // La mejor solución está en la primera posición del array
+          fitness_actual = poblacion[0].fitness;
+          diferencia_fitness = -(fitness_actual-fitness_anterior)/fitness_actual*100;
+
+          // Guardar cada 300 iteraciones para observar el progreso
+          if (PRINT) {
+            printf("Generacion %d - ", g);
+            printf("Fitness = %e - ", fitness_actual);
+            printf("Diferencia con Fitness Anterior = %.2e%c\n", diferencia_fitness, 37);
+            // if ((g % 300) == 0) {
+            //   printf("%s\n",output_file);
+            //   sprintf(output_file2,"image_%d.ppm",g);
+            //   escribir_ppm(output_file2, ancho, alto, max, poblacion[0]->imagen);
+            // }
+          }
+
+          offset = 0;
+          chunk_size = tam_poblacion / world_size;
+          scatterPoblacion(&offset, chunk_size, leftover, poblacion, NULL);
+          chunk_size += leftover;
+        } else {
+          gatherPoblacion(&offset, chunk_size, NULL, poblacion);
+
+          // Recibe la población ordenada
+          scatterPoblacion(&offset, chunk_size, leftover, NULL, poblacion);
+        }
+      } else {
+        qsort(poblacion, chunk_size, sizeof(Individuo), comp_fitness);
+      }
+    }
+  if(rank == MASTER) {
+    // Devuelve la imagen resultante
+    memmove(imagen_resultado, poblacion[0].imagen, num_pixels*sizeof(RGB));
+  } 
+  free(poblacion);
+}
+
+
+void cruzar(Individuo *padre1, Individuo *padre2, Individuo *hijo1, Individuo *hijo2, int num_pixels)
+{
+	// Elegir un "punto" de corte aleatorio a partir del cual se realiza el intercambio de los genes.
+	// * Cruzar los genes de cada padre con su hijo
+	// * Intercambiar los genes de cada hijo con los del otro padre
+	int corte = aleatorio(num_pixels - 1);
+
+	for(int i = 0; i < corte; i++) {
+		hijo1->imagen[i] = padre1->imagen[i];
+		hijo2->imagen[i] = padre2->imagen[i];
+	}
+
+	for(int i = corte; i < num_pixels; i++) {
+		hijo1->imagen[i] = padre2->imagen[i];
+		hijo2->imagen[i] = padre1->imagen[i];
+	}
+
+}
+
+void fitness(const RGB *objetivo, Individuo *individuo, int num_pixels)
+{
+	// Determina la calidad del individuo (similitud con el objetivo)
+	// calculando la suma de la distancia existente entre los pixeles
+  /* if(rank != MASTER) { */
+  /*   printf("Fitness - %d\n", rank); */
+  /*   printf("Pixel: %d\n", objetivo[1].r); */
+  /*   printf("Ind: %d\n", individuo->imagen[1].r); */
+  /* } */
+	double fitness = 0;
+	for(int i = 0; i < num_pixels; i++) {
+
+		fitness += abs(objetivo[i].r - individuo->imagen[i].r) + abs(objetivo[i].g - individuo->imagen[i].g) + abs(objetivo[i].b - individuo->imagen[i].b);
+	}
+
+	individuo->fitness = fitness;
+}
+
+void mutar(Individuo *actual, int max, int num_pixels)
+{
+	// Cambia el valor de algunos puntos de la imagen de forma aleatoria.
+	
+	// Decidir cuantos pixels mutar. Si el valor es demasiado pequeño,
+	// la convergencia es muy pequeña, y si es demasiado alto diverge.
+	double ratioMutacion = 0.002;
+	int numMutar = (int) num_pixels * ratioMutacion;
+
+	for(int i = 0; i < numMutar; i++) {
+		int index = aleatorio(num_pixels - 1);
+
+		actual->imagen[index].r = aleatorio(max);
+		actual->imagen[index].g = aleatorio(max);
+		actual->imagen[index].b = aleatorio(max);
+	}
+
+}
---
+++ src/imagen.c
@@ -0,0 +1,150 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <math.h>
+#include <mpi.h>
+#include "../include/derivados_mpi.h"
+#include "../include/imagen.h"
+
+extern int rank, world_size;
+extern MPI_Datatype rgb_type, individuo_type;
+
+RGB *leer_ppm(const char *file, int *ancho, int *alto, int *max)
+{
+	int i, n;
+	FILE *fd;
+	
+	char c, b[100];
+	int red, green, blue;
+	
+	fd = fopen(file, "r");
+	
+	n = fscanf(fd, "%[^\n] ", b);
+	if(b[0] != 'P' || b[1] != '3') {
+		printf("%s no es una imagen PPM\n", file);
+		exit(0);
+	}
+	
+	printf("Leyendo fichero PPM %s\n", file);
+	n = fscanf(fd, "%c", &c);
+	while(c == '#') {
+		n = fscanf(fd, "%[^\n] ", b);
+		printf("%s\n", b);
+		n = fscanf(fd, "%c", &c);
+	}
+	
+	ungetc(c, fd);
+	n = fscanf(fd, "%d %d %d", ancho, alto, max);
+	assert(n == 3);
+	
+	int size = (*ancho)*(*alto);
+	
+	RGB *imagen = (RGB *) malloc(size*sizeof(RGB));
+	assert(imagen);
+	
+	for(i=0; i<size; i++) {
+		n = fscanf(fd, "%d %d %d", &red, &green, &blue);
+		assert(n == 3);
+		
+		imagen[i].r = red;
+		imagen[i].g = green;
+		imagen[i].b = blue;
+	}
+	
+	fclose(fd);
+	return imagen;
+}
+
+void escribir_ppm(const char *fichero, int ancho, int alto, int max, const RGB *imagen)
+{
+	int i;
+	FILE *fd;
+	
+	fd = fopen(fichero, "w");
+	
+	fprintf(fd, "P3\n");
+	fprintf(fd, "%d %d\n%d\n", ancho, alto, max);
+	
+	int size = alto*ancho;
+	for(i=0; i<size; i++) {
+		fprintf(fd, "%d %d %d ", imagen[i].r, imagen[i].g, imagen[i].b);
+		if((i+1) % 18 == 0) { fprintf(fd, "\n"); }
+	}
+	fclose(fd);
+}
+
+void suavizar(int ancho, int alto, RGB *imagen)
+{
+	// Aplicar tecnica "mean-filter" para suavizar la imagen resultante
+	int r = 0, g = 0, b = 0;
+	//	(i-1, j-1) | (i-1, j) | (i-1, j+1)
+	//	(i, j-1)   | p(i,j)   | (i, j+1)
+	//	(i+1, j-1) | (i+1, j) | (i+1, j+1)
+  MPI_Status status;
+  int chunk_size = alto / world_size;
+  int leftover = alto % world_size;
+  int offset;
+
+  if(rank == 0) {
+    offset = ancho*(chunk_size + leftover);
+    for(int i = 1; i < world_size; i++) {
+      MPI_Send(&offset, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+      MPI_Send(&(imagen[offset]), ancho*chunk_size, rgb_type, i, 1, MPI_COMM_WORLD);
+      offset += ancho * chunk_size;
+    }
+    chunk_size += leftover;
+  } else {
+    imagen = (RGB *) malloc(ancho * chunk_size * sizeof(RGB));
+    MPI_Recv(&offset, 1, MPI_INT, MASTER, 0, MPI_COMM_WORLD, &status);
+    MPI_Recv(imagen, ancho*chunk_size, rgb_type, MASTER, 1, MPI_COMM_WORLD, &status);
+  }
+
+	for(int i = 1; i < chunk_size - 1; i++) {
+		for(int j = 1; j < ancho - 1; j++) {
+			r = imagen[(i-1)*ancho + j-1].r +  imagen[(i-1)*ancho + j].r +  imagen[(i-1)*ancho + j+1].r + 
+			imagen[i*ancho + j-1].r + imagen[i*ancho + j].r + imagen[i*ancho + j+1].r + 
+			imagen[(i+1)*ancho + j-1].r +  imagen[(i+1)*ancho + j].r +  imagen[(i+1)*ancho + j+1].r;
+
+			g = imagen[(i-1)*ancho + j-1].g +  imagen[(i-1)*ancho + j].g +  imagen[(i-1)*ancho + j+1].g +
+			imagen[i*ancho + j-1].g + imagen[i*ancho + j].g + imagen[i*ancho + j+1].g +
+			imagen[(i+1)*ancho + j-1].g +  imagen[(i+1)*ancho + j].g +  imagen[(i+1)*ancho + j+1].g;
+
+			b = imagen[(i-1)*ancho + j-1].b +  imagen[(i-1)*ancho + j].b +  imagen[(i-1)*ancho + j+1].b +
+			imagen[i*ancho + j-1].b + imagen[i*ancho + j].b + imagen[i*ancho + j+1].b +
+			imagen[(i+1)*ancho + j-1].b +  imagen[(i+1)*ancho + j].b +  imagen[(i+1)*ancho + j+1].b;
+
+			imagen[i*ancho + j].r = round(r/9);
+			imagen[i*ancho + j].g = round(g/9);
+			imagen[i*ancho + j].b = round(b/9);
+		}
+	}
+  /* Bordes superior e inferior */
+  for (int j = 1; j < ancho - 1; j++) {
+    imagen[j] = imagen[j + ancho];
+    imagen[ancho*(chunk_size-1)+j] = imagen[ancho*(chunk_size-1)+ j - ancho];
+  }
+
+  if(rank == MASTER) {
+    chunk_size -= leftover;
+    for(int i = 1; i < world_size; i++) {
+      MPI_Recv(&offset, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+      MPI_Recv(&(imagen[offset]), ancho * chunk_size, rgb_type, status.MPI_SOURCE, 1, MPI_COMM_WORLD, &status);
+    }
+    // Suavizar bordes
+    /* Esquinas */
+    imagen[0] = imagen[ancho+1];
+    imagen[ancho-1] = imagen[ancho*2 + 2];
+    imagen[ancho*(alto-1)] = imagen[ancho*(alto-1) - ancho + 1];
+    imagen[ancho*alto - 1] = imagen [ancho*alto - ancho - 2];
+
+    /* Bordes izquierda y derecha */
+    for (int i = 1; i < alto - 1; i++) {
+      imagen[ancho*i] = imagen[ancho*i + 1];
+      imagen[ancho*i - 1] = imagen[ancho*i - 2];
+    }
+
+  } else {
+    MPI_Send(&offset, 1, MPI_INT, MASTER, 0, MPI_COMM_WORLD);
+    MPI_Send(imagen, ancho * chunk_size, rgb_type, MASTER, 1, MPI_COMM_WORLD);
+  }
+}
